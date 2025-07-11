9ff6a936648c622d02e700ab265eb1a97a306aba
outcome9k <vpnv92497@gmail.com>
2025-07-12 00:49:35 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 from setuptools import setup, find_packages
+
 setup(
     name='test',
     version='0.1.0',
-    description='Example CLI Obfuscator Tool Menu',
+    description='Tool Menu CLI',
     author='Outcome9k',
-    author_email='you@example.com',
-    url='https://github.com/outcome9k/test',
     packages=find_packages(),
-    python_requires='>=3.6',
     entry_points={
         'console_scripts': [
-            'toolmenu = test.main:main',  # <command> = <module>:<function>
+            'toolmenu = test.main:main',
         ]
     },
+    python_requires='>=3.6',
 )
