71309324e9ca46bb4504cf715401b3e5776f0562
outcome9k <vpnv92497@gmail.com>
2025-07-14 02:29:11 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,20 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name="toolmenu",
     version="1.0",
-    author="outcome9k",
-    description="CLI tool to manage and run obfuscation tools from GitHub",
-    packages=find_packages(),  # Finds 'test' package
-    install_requires=[
-        "requests"
-    ],
+    packages=find_packages(),  # will detect 'toolmenu' folder
     entry_points={
         "console_scripts": [
-            "toolmenu = test.main:main"  # maps 'toolmenu' command to main() in test/main.py
+            "toolmenu = toolmenu.main:main"
         ]
     },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License"
-    ],
-    python_requires='>=3.6',
+    install_requires=["requests"],
+    python_requires=">=3.6"
 )
